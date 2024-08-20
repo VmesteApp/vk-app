6ef7466c2c40d7761d8db5818f631849a3217fce
your Name <qwertyshurazh@gmail.com>
2024-08-20 21:19:58 +0300

    remove old files

--- src/panels/index.ts
+++ src/panels/index.ts
@@ -1,9 +1,5 @@
-export { Persik } from "./Persik";
-export { Home } from "./Home";
 export { Feed } from "./Feed";
 export { MyProjects } from "./MyProjects";
 export { Profile } from "./Profile";
 export { Project } from "./Project";
-export { Notifications } from './Notifications'
-
-export type { HomeProps } from "./Home";
+export { Notifications } from "./Notifications";
