024c034271ec84e55759ca3d55d54e4241c7a655
your Name <qwertyshurazh@gmail.com>
2024-08-22 01:16:46 +0300

    Add MyPulses tabbar

--- src/panels/MyPulses.tsx
+++ src/panels/MyPulses.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, ReactNode } from "react";
 import {
   Panel,
   PanelHeader,
@@ -6,9 +6,45 @@ import {
   PanelHeaderBack,
   Group,
   Placeholder,
+  Tabs,
+  HorizontalScroll,
+  TabsItem,
 } from "@vkontakte/vkui";
-import { Icon28ServicesOutline } from "@vkontakte/icons";
+import {
+  Icon24Advertising,
+  Icon24HomeOutline,
+  Icon24Like,
+  Icon28ServicesOutline,
+} from "@vkontakte/icons";
 import { useRouteNavigator } from "@vkontakte/vk-mini-apps-router";
+
+type TabType = {
+  icon: ReactNode;
+  title: string;
+  panel: string;
+  path: string;
+};
+
+const tabs: TabType[] = [
+  {
+    icon: <Icon24Advertising />,
+    title: "Новый Импульс",
+    path: "",
+    panel: "",
+  },
+  {
+    icon: <Icon24HomeOutline />,
+    title: "Мои Импульсы",
+    path: "",
+    panel: "",
+  },
+  {
+    icon: <Icon24Like />,
+    title: "Заявки",
+    path: "",
+    panel: "",
+  },
+];
 
 export const MyPulses: FC<NavIdProps> = ({ id }) => {
   const routeNavigator = useRouteNavigator();
@@ -21,6 +57,27 @@ export const MyPulses: FC<NavIdProps> = ({ id }) => {
         Мои Импульсы
       </PanelHeader>
 
+      <Group>
+        <Tabs
+          mode={"default"}
+          layoutFillMode={"auto"}
+          withScrollToSelectedTab
+          scrollBehaviorToSelectedTab="center"
+        >
+          <HorizontalScroll arrowSize="m">
+            {tabs.map((tab) => (
+              <TabsItem
+                selected={tab.panel === "groups"}
+                disabled={false}
+                onClick={() => {}}
+                before={tab.icon}
+              >
+                {tab.title}
+              </TabsItem>
+            ))}
+          </HorizontalScroll>
+        </Tabs>
+      </Group>
       <Group style={{ height: "1000px" }}>
         <Placeholder
           icon={<Icon28ServicesOutline width={56} height={56} />}
