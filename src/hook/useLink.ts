6f0b1a1cd1af5141a60e375962dc4b7242ec3145
your Name <qwertyshurazh@gmail.com>
2024-11-09 02:36:08 +0300

    fix openLink function

--- src/hook/useLink.ts
+++ src/hook/useLink.ts
@@ -1,12 +1,17 @@
+import { parseURLSearchParamsForGetLaunchParams } from "@vkontakte/vk-bridge";
 import { usePlatform } from "@vkontakte/vkui";
 
 export const useLink = () => {
   const platform = usePlatform();
+  const { vk_platform } = parseURLSearchParamsForGetLaunchParams(
+    window.location.search
+  );
 
   const openLink = (href: string) => {
     const link = document.createElement("a");
     link.href = href;
-    link.target = platform === "vkcom" ? "_black" : "_self";
+    link.target =
+      platform === "vkcom" || vk_platform === "mobile_web" ? "_black" : "_self";
 
     document.body.appendChild(link);
 
