1e9cf89f99071eaa8409aa6948902f56afe97bec
your Name <qwertyshurazh@gmail.com>
2024-11-30 23:44:45 +0300

    add silent auth

--- src/utils/authorize.ts
+++ src/utils/authorize.ts
@@ -1,26 +1,13 @@
 import bridge from "@vkontakte/vk-bridge";
-import {
-  APP_ID,
-  BASE_URL,
-  VK_ACCESS_TOKEN,
-  VMESTE_ACCESS_TOKEN,
-  VMESTE_USER_ID,
-} from "../constants";
+import { BASE_URL, VMESTE_ACCESS_TOKEN, VMESTE_USER_ID } from "../constants";
 import { setStorageValue } from "./vkStorage";
 
 export async function authorize() {
   try {
-    const vkToken = await bridge.send("VKWebAppGetAuthToken", {
-      app_id: APP_ID,
-      scope: "",
-    });
-
-    if (vkToken && vkToken.access_token) {
-      setStorageValue(VK_ACCESS_TOKEN, vkToken.access_token);
-
+    if (window.location.href) {
       const request = await fetch(`${BASE_URL}/auth/login/vk`, {
         method: "POST",
-        body: JSON.stringify({ vkAccessToken: vkToken.access_token }),
+        body: JSON.stringify({ vkLaunchParams: window.location.href }),
       });
 
       if (request.ok) {
